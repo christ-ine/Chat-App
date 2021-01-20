5a9eeff61ea7fb3153c62aa89a52b9cf0be0fef8
Christine Ross <christinejoyross@gmail.com>
2021-01-19 16:56:51 -0800

    user enter and and left message

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+//get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
+
+//join chatroom
+socket.emit('joinRoom', {username, room } );
+
+//Get room and users
+socket.on('roomUsers', ({ room, users }) =>{
+    outputRoomName(room);
+    outputUsers(users);
+})
 
 //Message from server
 socket.on('message', message => {
@@ -31,9 +47,12 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message){
     const div = document.createElement('div')
     div.classList.add('message');
-    div.innerHTML = `<p class="meta">User <span>time</span></p>
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-     ${message}
+     ${message.text}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div)
 }
+
+//Add room name to DOM
+function outputRoomName(room)
